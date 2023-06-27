f7eae19e68c97ff048ef6f743b4c964aa9013be0
Danny <dannymai20@yahoo.com>
2023-06-27 18:14:11 -0400

    implement logic for color change on square

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for (let i = 0; i < 256; i++){
     let square = document.createElement('div');
     container.appendChild(square);
 }
+
+container.addEventListener('mouseover', (e) => {
+    if(e.target !== container){
+        e.target.classList.add('color-active');
+    }
+})
+
