bf1988624f22d01e0d865c16020ba63988a200e9
Danny <dannymai20@yahoo.com>
2023-07-06 11:26:55 -0400

    finish reset board

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let container = document.querySelector('.container');
+let newGrid = document.querySelector('.newGrid');
 
 for (let i = 0; i < 256; i++){
     let square = document.createElement('div');
+    square.classList.add('square')
     container.appendChild(square);
 }
 
@@ -10,4 +12,18 @@ container.addEventListener('mouseover', (e) => {
         e.target.classList.add('color-active');
     }
 })
+
+newGrid.addEventListener('click', () => {
+    // let gridSize = prompt('What grid size would you like?');
+    //     if(gridSize >= 100){
+    //         alert('please choose a smaller grid size');
+    //         return;
+    //     }
+
+    let element = document.getElementById('square');
+    element.remove();
+
+
+})
+
 
