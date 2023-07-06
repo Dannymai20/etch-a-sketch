a5be3ff2eb537ab2acb53eb0159ffcb67653036a
Danny <dannymai20@yahoo.com>
2023-07-06 11:58:30 -0400

    adjust square size based on number of squares

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let container = document.querySelector('.container');
 let newGrid = document.querySelector('.newGrid');
 
-for (let i = 0; i < 256; i++){
-    let square = document.createElement('div');
-    square.classList.add('square')
-    container.appendChild(square);
+let createGrid = function(squares){
+
+    squareSize = 480 / squares; // Calculate the size of each square based on the number input
+
+    for (let i = 0; i < squares * squares; i++){
+        let square = document.createElement('div');
+        square.classList.add('square')
+        container.appendChild(square);
+        square.style.width = `${squareSize}px`; // Set the width of each square
+        square.style.height = `${squareSize}px`; // Set the height of each square
+    }
 }
+
+createGrid(60);
 
 container.addEventListener('mouseover', (e) => {
     if(e.target !== container){
@@ -14,16 +23,22 @@ container.addEventListener('mouseover', (e) => {
 })
 
 newGrid.addEventListener('click', () => {
-    // let gridSize = prompt('What grid size would you like?');
-    //     if(gridSize >= 100){
-    //         alert('please choose a smaller grid size');
-    //         return;
-    //     }
+     let gridSize = prompt('What grid size would you like?');
+         if(gridSize >= 100){
+             alert('please choose a smaller grid size');
+             return;
+         }
 
-    let element = document.getElementById('square');
-    element.remove();
+    //gets elements of same class names
+    let elements = container.getElementsByClassName('square');
+    let squareArray = Array.from(elements);
 
+    squareArray.forEach((square) => {
+        square.remove();
+    });
 
-})
+    createGrid(gridSize);
+
+});
 
 
